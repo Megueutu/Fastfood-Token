06146cb92c535b508b723c09b57d4b7767bcbe78
daaavi4 <davi.alves@picpay.com>
2026-04-13 15:14:04 -0300

    build: Springboot project initialized

---
+++ src/main/java/com/fastfoood/fastfoood/FastfooodApplication.java
@@ -0,0 +1,13 @@
+package com.fastfoood.fastfoood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfooodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfooodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastfoood/fastfoood/FastfooodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastfoood.fastfoood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfooodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
